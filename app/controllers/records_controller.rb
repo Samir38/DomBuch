7b188c62c5dff2548a04413bb84bc7d41852ae41
Sergey Kintsel <serjonya@mail.ru>
2014-04-29 11:58:10 +0700

    some fixes again

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,14 +1,15 @@
 class RecordsController < ApplicationController
   before_action :set_record, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+
   def index
     if category = current_user.categories.find_by(name: params[:category])
       @records = current_user.records.where(category: category)
     else
       @records = current_user.records.all
     end
-    @records = @records.sort { |a,b| b.date <=> a.date }
-    @categories = current_user.categories.all
+    @records = @records.sort { |a, b| b.date <=> a.date }
+    @categories = current_user.categories.all.map { |x| x if x.records.size > 0 }.delete_if { |x| x.nil? }
     @summ = Record.all.pluck(:sum, :kind).inject(0) { |result, x| result += x.last == 'Кредит' ? x.first : -x.first }
   end
 
@@ -67,13 +68,13 @@ class RecordsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_record
-      @record = current_user.records.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_record
+    @record = current_user.records.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def record_params
-      params.require(:record).permit(:desc, :sum, :date, :kind)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def record_params
+    params.require(:record).permit(:desc, :sum, :date, :kind)
+  end
 end
